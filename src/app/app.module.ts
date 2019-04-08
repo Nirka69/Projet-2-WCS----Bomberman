fa965fbed8f65307815f7f5d0c26289156a021f6
Michael M <williamkailani@gmail.com>
2019-04-08 14:06:58 +0200

    integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
 import { MapComponent } from './map/map.component';
 import { GameComponent } from './game/game.component';
 
@@ -17,7 +11,6 @@ import { GameComponent } from './game/game.component';
     AppComponent,
     MapComponent,
     GameComponent
->>>>>>> dev
   ],
   imports: [
     BrowserModule,
