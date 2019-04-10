16bc9696504ff70671ed2021c3a7ac67f541ece8
Karakog Davut <khd69@yahoo.com>
2019-04-10 16:30:04 +0200

    maj avec debut boucle

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,9 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  border = {
+    textureUrl: "/assets/img/PierreWall.jpg",
+    breakable: false,
+    powerUp: false
+  };
+
+  floor = {
+    textureUrl: "/assets/img/Floor1.jpg",
+    breakable: false,
+    powerUp: false
+  };
+
+  wallFix = {
+    textureUrl: "/assets/img/Pierrewall.jpg",
+    breakable: false,
+    powerUp: false
+  };
+
+  wallDestructible = {
+    textureUrl: "/assets/img/Box.jpg",
+    breakable: true,
+    powerUp: true
+  };
+
+  textures= [this.border,
+  this.floor,
+  this.wallFix,
+  this.wallDestructible];
+
+  map =
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0],
+      [0, 1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 1, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 0],
+      [0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0],
+      [0, 1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 1, 0],
+      [0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    private nbColonne = 23;
+    private nbLigne = 17;
+    private tableauMur = [];
+    
+    constructor() {
+      for (let i = 0; i < this.nbLigne; i++) {
+        const gridRow = i + 1;
+  
+        for (let j = 0; j < this.nbColonne; j++) {
+          const gridColumn = j + 1;
+  
+          this.tableauMur.push([gridRow, gridColumn]);
+        }
+      }
+      console.log(this.tableauMur);
+    }
 
   ngOnInit() {
   }
 
+  
 }
