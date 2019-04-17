b4970163610d522ce8a8172818a6d9ce494755b5
anthoiervasi <>
2019-04-17 16:37:44 +0200

    modif du gamestate

--- src/app/game-state.service.ts
+++ src/app/game-state.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
 
 
-export const Move_top = 1;
-export const Move_Right = 2;
-export const Move_Bot = 3;
-export const Move_Left = 4;
-export const Drop_Bomb = 5;
-export const Bonus_speed = 6;
-export const Bonus_power = 7;
-export const Bonus_Bomb_Number = 8;
+export const MOVE_TOP = 1;
+export const MOVE_RIGHT = 2;
+export const MOVE_BOT = 3;
+export const MOVE_LEFT = 4;
+export const DROP_BOMB = 5;
+export const BONUS_SPEED = 6;
+export const BONUS_POWER = 7;
+export const BONUS_BOMB_NUMBER = 8;
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameStateService {
 
-public move : any = 0;
-public x : any = 0;
-public y : any = 0;
+  character = {
+    player: [],
+    bomb_Number: [],
+    bomb_Power: [],
+    move_Speed: []
+  }
 
-  constructor() { 
-   
-}}
+  player = [
+    { id: 'perso1', src: '' },
+    { id: 'perso2', src: '' },
+    { id: 'perso3', src: '' },
+    { id: 'perso4', src: '' }
+  ];
+
+  bomb_Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  bomb_Power = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  move_Speed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  constructor() {
+
+  }
+}
