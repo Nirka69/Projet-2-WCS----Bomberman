023741c31fc09fea15771a4b762f8b2dc4859870
Michael M <williamkailani@gmail.com>
2019-04-24 17:43:13 +0200

    2 persos

--- src/app/game-state.service.ts
+++ src/app/game-state.service.ts
@@ -10,6 +10,12 @@ export const DROP_BOMB = 5;
 export const BONUS_SPEED = 6;
 export const BONUS_POWER = 7;
 export const BONUS_BOMB_NUMBER = 8;
+export const MOVE_STAND2 = 9;
+export const MOVE_TOP2 = 10;
+export const MOVE_RIGHT2 = 11;
+export const MOVE_BOT2 = 12;
+export const MOVE_LEFT2 = 13;
+export const DROP_BOMB2 = 14;
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +27,25 @@ export class GameStateService {
   move: number;
   bomb = 0;
   bombList : Bomb[] = [];
+
+  player1 = {
+    charX: 1,
+    charY: 1,
+    move: 0,
+    bomb: 0,
+    bombList: [],
+  };
+
+
+player2={ 
+  charX : 21,
+  charY : 1,
+  move:  0,
+  bomb : 0,
+  bombList : [],
+};
+
+
 
 
   /* player = [
