345082e18eab1f9cc8572073d942c17d5d3f6406
Karakog Davut <khd69@yahoo.com>
2019-04-25 20:16:07 +0200

    rajout explosion dans class Bomb

--- src/app/models/bomb.ts
+++ src/app/models/bomb.ts
@@ -1,8 +1,9 @@
 export class Bomb {
 
- constructor(public positionX: number,
-             public positionY: number,
-             public date: Date,
-             public power = 1,
-             public playerId: number) {}
+    constructor(public positionX: number,
+                public positionY: number,
+                public date: Date,
+                public power = 1,
+                public playerId: number,
+                public explosion: boolean = false) { }
 }
