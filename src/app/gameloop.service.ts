2b98b37f04ea6bfdb437d4979a894a36a75d38f9
Karakog Davut <khd69@yahoo.com>
2019-05-01 15:30:42 +0200

    maj

--- src/app/gameloop.service.ts
+++ src/app/gameloop.service.ts
@@ -88,7 +88,7 @@ export class GameloopService {
   }
   dropBomb() {
     if (this.gs.player1.bomb === DROP_BOMB && this.gs.player1.maxBomb > this.gs.player1.bombList.length) {
-      let bomb = new Bomb(this.gs.player1.charX, this.gs.player1.charY, new Date(), 5, 0)
+      let bomb = new Bomb(this.gs.player1.charX, this.gs.player1.charY, new Date(), this.gs.counterValue, 0)
       this.gs.player1.bombList.push(bomb)
       this.gs.player1.bomb = 0;
       this.dropsound = new Audio()
